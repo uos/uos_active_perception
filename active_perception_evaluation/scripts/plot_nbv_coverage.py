833835a00fd8cc6154aa8a39199e66e098d7c65f
Thorsten Gedicke <tgedicke@uos.de>
2015-04-14 19:25:32 +0200

    update nbv sample eval

--- active_perception_evaluation/scripts/plot_nbv_coverage.py
+++ active_perception_evaluation/scripts/plot_nbv_coverage.py
@@ -14,35 +14,32 @@ import common
 
 def main():
 
-    color = "blue"
-    showtime = True
+    files = ["/home/thorsten/ros_fuerte/race/uos_active_perception/race_object_search/config/maps/nbveval.csv",
+             "/home/thorsten/ros_fuerte/race/uos_active_perception/race_object_search/config/maps/nbveval75.csv"]
+    colors = common.graphColors(len(files))
+    labels = ["0.0", "0.75"]
+    showtime = False
+    relmax = 17226
 
-    cellcounts = []
-    meantimes = []
-    ymax = 0
-    n = sys.maxint
-    with open("/home/thorsten/ros_fuerte/race/uos_active_perception/race_object_search/config/maps/nbveval.csv") as df:
-        for line in df:
-            elems = line.split(",")
-            if elems[0] == "cells":
-                cells = np.array([float(x) for x in elems[1:]])
-                cellcounts.append(cells)
-                ymax = max(ymax, cells[-1])
-                n = min(n, len(cells))
-            if elems[0] == "response":
-                meantimes.append(float(elems[1]) / float(elems[2]))
-    time_per_sample = np.mean(meantimes)
+    setup()
+    for i in range(len(files)):
+        plotfile(files[i], colors[i], labels[i], showtime, relmax)
 
-    means = np.zeros(n)
-    stds = np.zeros(n)
-    for i in range(n):
-        vals = [cells[i] for cells in cellcounts]
-        means[i] = np.mean(vals)
-        stds[i] = np.std(vals)
+    if showtime:
+        pylab.xlabel('time [s]', x=1, ha="right")
+    else:
+        pylab.xlabel('# samples', x=1, ha="right")
+    pylab.ylabel('coverage', y=1, va="top")
+    #pylab.title('Search progress over time', y=1.01, va="bottom")
 
+    pylab.legend(loc="lower right")
+    pylab.tight_layout()
+    pylab.ylim(0.0, pylab.ylim()[1])
+    pylab.show()
+
+def setup():
     # Common sizes: (10, 7.5) and (12, 9)
     pylab.figure(facecolor="white", figsize=(6, 3))
-
     # Remove the plot frame lines. They are unnecessary chartjunk.
     ax = pylab.subplot(111)
     ax.spines["top"].set_visible(False)
@@ -54,26 +51,43 @@ def main():
     # Gridlines
     ax.yaxis.grid(b=True, which='both', color='black', alpha=0.3, ls='--')
 
+def plotfile(path, color, label, showtime, relmax=1):
+    cellcounts = []
+    meantimes = []
+    ymax = 0
+    n = sys.maxint
+    with open(path) as df:
+        for line in df:
+            elems = line.split(",")
+            if elems[0] == "cells":
+                cells = np.array([float(x) for x in elems[1:]])
+                cellcounts.append(cells)
+                ymax = max(ymax, cells[-1])
+                n = min(n, len(cells))
+            if elems[0] == "response":
+                meantimes.append(float(elems[1]) / float(elems[2]))
+    time_per_sample = np.mean(meantimes)
+    print label
+    print "ymax:", ymax
+    print "time per sample:", time_per_sample
+
+    means = np.zeros(n)
+    stds = np.zeros(n)
+    for i in range(n):
+        vals = [cells[i] for cells in cellcounts]
+        means[i] = np.mean(vals) / relmax
+        stds[i] = np.std(vals) / relmax
+
     x = np.array(range(n))
     if showtime:
         x = [float(val) * time_per_sample for val in x]
-        print x
 
-    pylab.plot(x, means/ymax, ls="-", lw=2.0, color=color)
-    upper = (means + stds) / ymax
-    lower = (means - stds) / ymax
+    pylab.plot(x, means, ls="-", lw=2.0, color=color, label=label)
+    upper = (means + stds)
+    lower = (means - stds)
     pylab.plot(x, upper, ls="-", lw=2.0, color="grey")
     pylab.plot(x, lower, ls="-", lw=2.0, color="grey")
-    pylab.fill_between(x, upper, lower, color=color, alpha='0.3')
-
-    if showtime:
-        pylab.xlabel('time [s]', x=1, ha="right")
-    else:
-        pylab.xlabel('# samples', x=1, ha="right")
-    pylab.ylabel('coverage', y=1, va="top")
-    #pylab.title('Search progress over time', y=1.01, va="bottom")
-    pylab.tight_layout()
-    pylab.show()
+    pylab.fill_between(x, upper, lower, color=[color[0], color[1], color[2], 0.3])
 
 if __name__=="__main__":
     main()
